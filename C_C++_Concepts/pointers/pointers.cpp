3bc6266757d8b6f5870cd07a1bb52c5a5091a67e
Caio Henrique <caiohcjk@gmail.com>
2024-05-15 10:10:56 -0300

    tests made to learn pointers
    small codebits that i used to learn and understand what pointers do

---
+++ C_C++_Concepts/pointers/pointers.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+//accessing pointer in heap memory
+// int main () {
+//     int *P;
+
+//     P = new int[5];
+
+//     P[0] = 10; 
+//     P[1] = 34; 
+//     P[2] = 15; 
+//     P[3] = 29; 
+//     P[4] = 73; 
+
+//     for (int i = 0; i < 5; i++){
+//         cout << P[i] << endl;
+//     }
+
+//     delete [ ] P;
+// }
+
+//accessing pointer in stack
+// int main (){
+//     int a    = 20;
+//     int *P;
+//     P = &a;
+
+//     cout << a << endl;
+//     cout << P << endl;
+//     cout << *P <<endl;
+
+//     return 0;
+// }
